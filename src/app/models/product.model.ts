20900e31e31a54dc95945e5d1a127ec442d34c26
Yassin Chamlal <64170166+yassi22@users.noreply.github.com>
2024-03-21 10:28:59 +0100

    Update
    extra product details ophalen

--- src/app/models/product.model.ts
+++ src/app/models/product.model.ts
@@ -5,8 +5,17 @@ export class Product {
   public title: string;
   public imageurl: string;
   public description: string; 
-  public developer :string;
-  public releaseDate : string;
+  public developer :string; 
+  public publisher: string; 
+  public genre: number[]; 
+  public language: string[];  
+  public releaseDate : string; 
+  public os: string; 
+  public processor: string; 
+  public memory: string; 
+  public graphics: string; 
+  public sound_card: string; 
+
   public price: number;
   // public category: Category;
 } 
